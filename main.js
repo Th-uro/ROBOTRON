2b3c8fd5aa692abc383ecdabcd504f265caad443
Th-uro <arthurolivsria@gmail.com>
2023-03-02 22:51:59 -0300

    Adicionando novas formas de chamar uma função

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const robotron = document.querySelector('#robotron')
 
-document.addEventListener('click', dizOi)
+document.addEventListener('click', function () {
+  console.log('Cliquei no robo')
+})
 
 function dizOi() {
   console.log('oi')
+  console.log('Bem-Vindo ao Robotron 2000')
 }
+
+dizOi()
